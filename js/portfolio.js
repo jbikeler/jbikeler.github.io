974250fe1d42e26d89be0acd138134959d9e06e9
jbikeler <jbikeler@gmail.com>
2023-06-23 14:27:36 -0700

    Update Avatar FXs
    Added js for avatar to follow mouse

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,12 @@
-function scrollDown(section) {
-    //planed smooth scroll to next section on down scroll
-}
+const container = document.querySelector(".rotate-fx-con")
+const card = document.querySelector(".rotate-fx")
 
-function scrollUp(section) {
-    //planed smooth scroll to previous section on up scroll
-}
+container.addEventListener('mousemove', (e) => {
+    let rect = e.target.getBoundingClientRect();
+    let posX = e.clientX - rect.left;
+    let posY = e.clientY - rect.top;
+    let xAxis = (-(window.innerWidth / 3 - posX) /20);
+    let yAxis = ((window.innerHeight / 2 - posY) /20);
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    // console.log(e.pageX, e.pageY);
+});
