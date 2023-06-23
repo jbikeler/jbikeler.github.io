bc4d1f48d5cb42f5b9eb12b4d158439e8f31d872
jbikeler <jbikeler@gmail.com>
2023-06-23 11:42:28 -0700

    Update Portfolio
    Design V2, Added animations, Started JS

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+function scrollDown(section) {
+    //planed smooth scroll to next section on down scroll
+}
+
+function scrollUp(section) {
+    //planed smooth scroll to previous section on up scroll
+}
